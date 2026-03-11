3180a9cfbf08d475844a369857a1ea9099c81fe8
Murilo Lacerda <murilo.lacerda@eaportal.org>
2026-03-10 22:59:34 -0300

    atualizado2026

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+const menuToggle = document.getElementById("menu-toggle")
+const menu = document.getElementById("menu")
+
+menuToggle.addEventListener("click", () => {
+menu.classList.toggle("active")
+})
+
 const imagens = document.querySelectorAll(".carrossel img")
 
 imagens.forEach(img => {
-
 img.addEventListener("click", () => {
-
 img.classList.toggle("zoom")
-
 })
-
 })
